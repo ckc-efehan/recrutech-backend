ced6325fa8d215eda00b9f7a9621b0e218b65c03
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2025-09-02 13:03:23 +0200

    added new module for recrutech platform

---
+++ recrutech-services/recrutech-platform/src/main/java/com/recrutech/recrutechplatform/RecrutechPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.recrutech.recrutechplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutechPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecrutechPlatformApplication.class, args);
+    }
+
+}

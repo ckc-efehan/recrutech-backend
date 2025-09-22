e6d50bda801c5803be968606bb004cfe5d5eafaf
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2025-09-22 18:37:31 +0200

    updated

--- recrutech-services/recrutech-platform/src/test/java/com/recrutech/recrutechplatform/RecrutechPlatformApplicationTests.java
+++ recrutech-services/recrutech-platform/src/test/java/com/recrutech/recrutechplatform/RecrutechPlatformApplicationTests.java
@@ -2,8 +2,17 @@ package com.recrutech.recrutechplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.liquibase.enabled=false",
+        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=none"
+})
 class RecrutechPlatformApplicationTests {
 
     @Test

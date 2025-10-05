38bc982aa56ab39b40ad9ee6b9f1154a074a5dc9
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2025-10-05 21:45:26 +0200

    update

--- recrutech-services/recrutech-common/src/main/java/com/recrutech/common/entity/BaseEntity.java
+++ recrutech-services/recrutech-common/src/main/java/com/recrutech/common/entity/BaseEntity.java
@@ -24,7 +24,7 @@ import java.util.Objects;
 public abstract class BaseEntity {
 
     @Id
-    @Column(name = "id", columnDefinition = "CHAR(36)", nullable = false)
+    @Column(name = "ID", columnDefinition = "CHAR(36)", nullable = false, length = 36)
     protected String id;
 
     @Column(nullable = false)
